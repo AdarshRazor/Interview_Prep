aee023e5b4b33ba3e1cf345c213ef7598bf1631d
Adarsh Anand <adarshanshu7@gmail.com>
2025-01-06 05:23:38 +0530

    updated

--- ReactJS/reactHooks/hookreact/src/hooks/use-reducer/index.jsx
+++ ReactJS/reactHooks/hookreact/src/hooks/use-reducer/index.jsx
@@ -1,6 +1,23 @@
 import { useState } from "react"
 
 export const ReducerExample = () => {
+
+    const [state, dispatch]
+
+    return(
+        <div>
+            <p>Hey Reducer</p>
+            <p>Count: {count}</p>
+            <button onClick={()=> setCount((prev) => prev + 1)}>Increment</button>
+            <button onClick={()=> setCount((prev) => prev - 1)}>Decrement</button>
+        </div>
+    )
+}
+ 
+// using useState
+
+/*
+export const ReducerExample = () => {
     const [count, setCount] = useState(0)
     return(
         <div>
@@ -11,3 +28,4 @@ export const ReducerExample = () => {
         </div>
     )
 }
+*/
